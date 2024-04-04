9bddb22a24d2398f86c2b2d28b1ff285d9079059
rajesh-sv <125382031+rajesh-sv@users.noreply.github.com>
2024-04-04 18:42:56 +0530

    Add index page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Void Walker",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "DreamStealer",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Message Board", messages: messages });
 });
 
 module.exports = router;
