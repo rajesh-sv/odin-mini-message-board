996a02aa8ff81a0e398302337f31886a15da0c7e
rajesh-sv <125382031+rajesh-sv@users.noreply.github.com>
2024-04-04 19:42:38 +0530

    Add logic to add a new message to the index page

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,17 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Mini Message Board", messages: messages });
 });
 
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "Mini Message Board" });
+});
+
+router.post("/new", function (req, res, next) {
+  messages.push({
+    text: req.body.text,
+    user: req.body.user,
+    added: new Date(),
+  });
+  res.redirect("/");
+});
+
 module.exports = router;
